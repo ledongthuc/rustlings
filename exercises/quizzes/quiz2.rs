6dd7b2916a67da3ba86f25489b10d98772164212
Thuc Le <ledongthuc@gmail.com>
2026-07-13 22:48:57 +0700

    quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,24 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> { 
+        let mut result: Vec<String> = Vec::new();
+        for tupe_input in input {
+            let output = transformer_tupe(tupe_input);
+            result.push(output);
+        }
+        result
+    }
+
+    fn transformer_tupe(input: (String, Command)) -> String {
+        match input.1 {
+            Command::Uppercase => input.0.to_uppercase(),
+            Command::Trim => String::from(input.0.trim()),
+            Command::Append(size) => {
+                input.0 + &"bar".repeat(size)
+            },
+        }
+    }
 }
 
 fn main() {
@@ -37,7 +54,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer; 
     use super::Command;
 
     #[test]
